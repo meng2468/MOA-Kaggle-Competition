1b5f949a2c9abe8e5786860cc7cc670c38c15990
meng2468 <mandm116@gmail.com>
2020-10-25 09:50:38 +0100

    import test

--- data-processing/crossvalidation.py
+++ data-processing/crossvalidation.py
@@ -2,11 +2,5 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import KFold
 
-df_train_x = pd.read_csv('../processed-input/clean_train_features.csv')
-df_train_y = pd.read_csv('../processed-input/clean_train_targets.csv')
-
-skf = KFold(n_splits=5)
-skf.get_n_splits(df_train_x, df_train_y)
-
-for train_index, test_index in skf.split(df_train_x, df_train_y):
-    print(train_index, test_index)
+def add_fold_label(df_x, df_y):
+    
