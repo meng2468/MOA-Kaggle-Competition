177f42554275bbdbb2167f17e3d475ebc3a8e33f
CKHui <ckhanghui@gmail.com>
2020-11-12 00:10:52 +0800

    fix k_fold seed

--- processing/stratified_kfold.py
+++ processing/stratified_kfold.py
@@ -7,13 +7,13 @@ from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 # 2. import the library from input
 #    python > sys.path.append('../input/iterative-stratification/iterative-stratification-master')
 
-def get_stratified_kfold_index(train_targets, n_split=5):
+def get_stratified_kfold_index(train_targets, n_split=5, random_state=42):
     """
     return array with fold index when then data row is target
     """
     kfold_indexes = np.zeros(len(train_targets), dtype=int)
     placeholder = kfold_indexes.copy()
-    mskf = MultilabelStratifiedKFold(n_splits=n_split)
+    mskf = MultilabelStratifiedKFold(n_splits=n_split, random_state=random_state)
     for f, (t_idx, v_idx) in enumerate(mskf.split(X=placeholder, y=train_targets)):
         kfold_indexes[v_idx] = f
 
