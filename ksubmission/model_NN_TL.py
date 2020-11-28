2d581c19df435b7dd00b9c9e1636494626df1c24
CKHui <ckhanghui@gmail.com>
2020-11-28 18:57:13 +0800

    fix TL transform

--- ksubmission/model_NN_TL.py
+++ ksubmission/model_NN_TL.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self, num_features, num_targets):
         super(Model, self).__init__()
